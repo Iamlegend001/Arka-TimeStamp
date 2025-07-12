84be07ea45b98ec31161be9ff60f114d7673962e
Arka <arkapravasantra17@gmail.com>
2025-07-12 22:37:02 +0530

    @arkaprava/TimeStamp

---
+++ index.js
@@ -0,0 +1,25 @@
+function getTimeStamp() {
+  return new Date(), toISOString();
+}
+
+function getUnixTimeStamp() {
+  return Math.floor(Date.now() / 1000);
+}
+
+function formatTime(date = new Date()) {
+  return date.toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
+
+module.exports = {
+  getTimeStamp,
+  getUnixTimeStamp,
+  formatTime
+}
